de29673f0fa124b50dc6d6edc194d2358acfc3a3
ecm85 <ezramcnichols@gmail.com>
2014-05-27 01:08:12 -0500

    Extracted common threat logic to Threat.cs.

--- SpaceAlertResolver/BLL/Threats/ExternalThreat.cs
+++ SpaceAlertResolver/BLL/Threats/ExternalThreat.cs
@@ -5,49 +5,14 @@ using System.Text;
 
 namespace BLL.Threats
 {
-	public abstract class ExternalThreat
+	public abstract class ExternalThreat : Threat
 	{
-		private readonly int pointsForDefeating;
-		public int PointsForDefeating { get { return pointsForDefeating; } }
-		private readonly int pointsForSurviving;
-		public int PointsForSurviving { get { return pointsForSurviving; } }
-
-		private readonly int totalHealth;
-		public int TotalHealth { get { return totalHealth; } }
-		protected int remainingHealth;
-		public int RemainingHealth { get { return remainingHealth; } }
-		
-		protected int shields;
-
-		private readonly int speed;
-		public int Speed { get { return speed; } }
-
-		public abstract void PeformXAction(SittingDuck sittingDuck);
-		public abstract void PerformYAction(SittingDuck sittingDuck);
-		public abstract void PerformZAction(SittingDuck sittingDuck);
-
-		public int TimeAppears { get; private set; }
 		public ZoneType CurrentZoneType { get; private set; }
 
-		public virtual void TakeDamage(IList<Damage> damages)
+		protected ExternalThreat(int pointsForSurviving, int pointsForDefeating, int shields, int health, int speed, int timeAppears, ZoneType currentZoneType) :
+			base(pointsForSurviving, pointsForDefeating, shields, health, speed, timeAppears)
 		{
-			var damageDealt = damages.Sum(damage => damage.Amount) - shields;
-			if (damageDealt > 0)
-				remainingHealth -= damageDealt;
-		}
-
-		protected ThreatType threatType;
-		public ThreatType ThreatType { get { return threatType; } }
-
-		protected ExternalThreat(int pointsForSurviving, int pointsForDefeating, int shields, int health, int speed, int timeAppears, ZoneType currentZoneType)
-		{
-			this.pointsForSurviving = pointsForSurviving;
-			this.pointsForDefeating = pointsForDefeating;
-			this.shields = shields;
-			totalHealth = remainingHealth = health;
-			this.speed = speed;
 			CurrentZoneType = currentZoneType;
-			TimeAppears = timeAppears;
 		}
 	}
 }
---
+++ SpaceAlertResolver/BLL/Threats/Threat.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Threats
+{
+	public abstract class Threat
+	{
+		private readonly int pointsForDefeating;
+		public int PointsForDefeating { get { return pointsForDefeating; } }
+		private readonly int pointsForSurviving;
+		public int PointsForSurviving { get { return pointsForSurviving; } }
+
+		private readonly int totalHealth;
+		public int TotalHealth { get { return totalHealth; } }
+		protected int remainingHealth;
+		public int RemainingHealth { get { return remainingHealth; } }
+		
+		protected int shields;
+
+		private readonly int speed;
+		public int Speed { get { return speed; } }
+
+		public abstract void PeformXAction(SittingDuck sittingDuck);
+		public abstract void PerformYAction(SittingDuck sittingDuck);
+		public abstract void PerformZAction(SittingDuck sittingDuck);
+
+		public int TimeAppears { get; private set; }
+		
+
+		public virtual void TakeDamage(IList<Damage> damages)
+		{
+			var damageDealt = damages.Sum(damage => damage.Amount) - shields;
+			if (damageDealt > 0)
+				remainingHealth -= damageDealt;
+		}
+
+		protected ThreatType threatType;
+		public ThreatType ThreatType { get { return threatType; } }
+
+		protected Threat(int pointsForSurviving, int pointsForDefeating, int shields, int health, int speed, int timeAppears)
+		{
+			this.pointsForSurviving = pointsForSurviving;
+			this.pointsForDefeating = pointsForDefeating;
+			this.shields = shields;
+			totalHealth = remainingHealth = health;
+			this.speed = speed;
+			TimeAppears = timeAppears;
+		}
+	}
+}
