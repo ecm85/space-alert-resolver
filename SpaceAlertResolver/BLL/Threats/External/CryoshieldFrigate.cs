e71909ca6f7042e792e148c884592ccc69dee2c1
ecm85 <ezramcnichols@gmail.com>
2014-05-30 22:31:10 -0500

    Added cryoshield frigate.

---
+++ SpaceAlertResolver/BLL/Threats/External/CryoshieldFrigate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Threats.External
+{
+	public class CryoshieldFrigate : SeriousWhiteExternalThreat
+	{
+		private bool cryoshieldUp = true;
+
+
+		public CryoshieldFrigate(int timeAppears, Zone currentZone)
+			: base(1, 7, 3, timeAppears, currentZone)
+		{
+		}
+
+		public override void PeformXAction(SittingDuck sittingDuck)
+		{
+			sittingDuck.TakeAttack(2, CurrentZone);
+		}
+
+		public override void PerformYAction(SittingDuck sittingDuck)
+		{
+			sittingDuck.TakeAttack(3, CurrentZone);
+		}
+
+		public override void PerformZAction(SittingDuck sittingDuck)
+		{
+			sittingDuck.TakeAttack(4, CurrentZone);
+		}
+
+		public override void TakeDamage(IList<PlayerDamage> damages)
+		{
+			if (!damages.Any())
+				return;
+			if (cryoshieldUp) //TODO: Rules clarification: Does damage have to get through regular shield to take down cryoshield?
+				cryoshieldUp = false;
+			else
+				base.TakeDamage(damages);
+		}
+	}
+}
