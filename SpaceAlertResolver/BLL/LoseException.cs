c782a209f04d921aa04c9c466a66e6cecc552e02
ecm85 <ezramcnichols@gmail.com>
2014-06-01 19:11:48 -0500

    Untracked files AGAIN.

---
+++ SpaceAlertResolver/BLL/LoseException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BLL.Threats;
+
+namespace BLL
+{
+	public class LoseException : Exception
+	{
+		public Threat Threat { get; private set; }
+		public LoseException(Threat threat)
+		{
+			Threat = threat;
+		}
+	}
+}
---
+++ SpaceAlertResolver/BLL/ThreatDamageResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+	public class ThreatDamageResult
+	{
+		public int DamageDone { get; set; }
+		public bool ShipDestroyed { get; set; }
+
+		public void AddDamage(ThreatDamageResult other)
+		{
+			DamageDone += other.DamageDone;
+			ShipDestroyed = ShipDestroyed || other.ShipDestroyed;
+		}
+	}
+}
