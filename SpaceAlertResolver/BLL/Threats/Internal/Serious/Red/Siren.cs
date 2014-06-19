127fcaa709cc0d93200f7e66075a49a4f93a1828
ecm85 <ezramcnichols@gmail.com>
2014-06-19 01:31:31 -0500

    Added Siren.

---
+++ SpaceAlertResolver/BLL/Threats/Internal/Serious/Red/Siren.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BLL.ShipComponents;
+
+namespace BLL.Threats.Internal.Serious.Red
+{
+	public class Siren : SeriousRedInternalThreat
+	{
+		public Siren()
+			: base(2, 2, StationLocation.UpperRed, PlayerAction.BattleBots)
+		{
+		}
+
+		protected override void PerformXAction(int currentTurn)
+		{
+			var stationsWithOnePlayer = EnumFactory.All<StationLocation>()
+				.Where(stationLocation => stationLocation.IsOnShip())
+				.Where(stationLocation => SittingDuck.GetPlayerCount(stationLocation) == 1);
+			SittingDuck.KnockOutPlayers(stationsWithOnePlayer);
+		}
+
+		protected override void PerformYAction(int currentTurn)
+		{
+			var stationsWithMultiplePlayers = EnumFactory.All<StationLocation>()
+				.Where(stationLocation => stationLocation.IsOnShip())
+				.Where(stationLocation => SittingDuck.GetPlayerCount(stationLocation) > 1);
+			SittingDuck.KnockOutPlayers(stationsWithMultiplePlayers);
+		}
+
+		protected override void PerformZAction(int currentTurn)
+		{
+			SittingDuck.KnockOutPlayers(EnumFactory.All<StationLocation>());
+		}
+
+		public override void TakeDamage(int damage, Player performingPlayer, bool isHeroic, StationLocation? stationLocation)
+		{
+			base.TakeDamage(damage, performingPlayer, isHeroic, stationLocation);
+			if (!isHeroic)
+				performingPlayer.BattleBots.IsDisabled = true;
+			CurrentStation = StationLocation.LowerBlue;
+		}
+	}
+}
