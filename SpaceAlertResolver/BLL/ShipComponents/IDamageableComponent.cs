589fe25c6801f1be166319e4709d5618a1850777
ecm85 <ezramcnichols@gmail.com>
2014-06-23 18:29:16 -0500

    Missed file in last commit.

---
+++ SpaceAlertResolver/BLL/ShipComponents/IDamageableComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.ShipComponents
+{
+	public interface IDamageableComponent
+	{
+		void SetDamaged();
+		void Repair();
+	}
+}
