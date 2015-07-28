b6cb582cfe3caf8aeacebadca3378417f3af3b65
ecm85 <ezramcnichols@gmail.com>
2015-07-28 11:39:49 -0500

    Removed unused extension method.

--- SpaceAlertResolver/BLL/Threats/ThreatType.cs
+++ SpaceAlertResolver/BLL/Threats/ThreatType.cs
@@ -12,22 +12,4 @@ namespace BLL.Threats
 		SeriousExternal = 3,
 		MinorExternal = 4
 	}
-
-	public static class ThreatTypeExtensions
-	{
-		public static int ThreatPoints(this ThreatType threatType)
-		{
-			switch (threatType)
-			{
-				case ThreatType.MinorExternal:
-				case ThreatType.MinorInternal:
-					return 1;
-				case ThreatType.SeriousExternal:
-				case ThreatType.SeriousInternal:
-					return 2;
-				default:
-					throw new InvalidOperationException();
-			}
-		}
-	}
 }
