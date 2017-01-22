0a9a7557f9a6c9605b8b04659e820771834839e5
ezramcnichols <ezramcnichols@gmail.com>
2017-01-22 03:11:25 -0600

    Showed fuel capsules on the client.

--- SpaceAlertResolver/PL/Models/LowerWhiteStationModel.cs
+++ SpaceAlertResolver/PL/Models/LowerWhiteStationModel.cs
@@ -1,12 +1,16 @@
-﻿using BLL;
+﻿using System.Collections.Generic;
+using System.Linq;
+using BLL;
 using BLL.ShipComponents;
 
 namespace PL.Models
 {
 	public class LowerWhiteStationModel : StandardStationModel
 	{
+		public IEnumerable<int> FuelCapsules { get; set; }
 		public LowerWhiteStationModel(Game game) : base(game, StationLocation.LowerWhite)
 		{
+			FuelCapsules = Enumerable.Range(1, game.SittingDuck.WhiteZone.LowerWhiteStation.CentralReactor.FuelCapsules);
 		}
 	}
 }
