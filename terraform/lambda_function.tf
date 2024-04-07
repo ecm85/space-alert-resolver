ddbfdd140438cf5c06c2706031fb7b4e5d5c9e1d
Ezra McNichols <Ezra.McNichols@directsupply.com>
2024-04-06 19:55:48 -0500

    change zip path.

--- terraform/lambda_function.tf
+++ terraform/lambda_function.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "tfer--space-002D-alert-002D-resolver" {
   reserved_concurrent_executions = "-1"
   role                           = "arn:aws:iam::854713338508:role/service-role/space-alert-resolver-role-th1q8vfz"
   runtime                        = "dotnetcore3.1"
-  source_code_hash               = filebase64sha256("SpaceAlertResolver/PL/lambda.zip")
+  source_code_hash               = filebase64sha256("terraform/lambda.zip")
   timeout                        = "15"
 
   tracing_config {
