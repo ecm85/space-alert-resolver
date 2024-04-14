0c8fd1d545d81007e6e394cab161a4b5345844dd
Ezra McNichols <Ezra.McNichols@directsupply.com>
2024-04-13 22:16:38 -0500

    what space.

--- terraform/api/iam.tf
+++ terraform/api/iam.tf
@@ -10,7 +10,7 @@ resource "aws_iam_policy" "lambda_execution_policy" {
         "logs:CreateLogGroup",
         "logs:CreateLogStream",
         "logs:PutLogEvents",
-        "ses: SendEmail"
+        "ses:SendEmail"
       ],
       "Effect": "Allow",
       "Resource": ["*"]
