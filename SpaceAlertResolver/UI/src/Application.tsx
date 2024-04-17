9edf12c234003d0fb7757c8810bf03b358d03377
Ezra McNichols <Ezra.McNichols@directsupply.com>
2024-04-16 23:11:06 -0500

    use back camera,, not front

--- SpaceAlertResolver/UI/src/Application.tsx
+++ SpaceAlertResolver/UI/src/Application.tsx
@@ -97,10 +97,10 @@ export default function Application() {
 			const camera2IsBack = cameraIsBack(cameraInfo2);
 
 			if (camera1IsBack && !camera2IsBack) {
-				return 1;
+				return -1;
 			}
 			if (camera2IsBack && !camera1IsBack) {
-				return -1;
+				return 1;
 			}
 
 			return 0;
