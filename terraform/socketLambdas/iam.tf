f17011d12f564389e9892c42754169ed0d9ee303
Ezra McNichols <Ezra.McNichols@directsupply.com>
2024-04-25 10:13:04 -0500

    Moved to lambdas, mark V.

--- terraform/socketLambdas/iam.tf
+++ terraform/socketLambdas/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "lambda_execution_policy" {
-  name = "space-alert-api-lambda-execution-role"
+  name = "websocket-lambda-execution-role"
   path = "/service-role/"
 
   policy = <<POLICY
